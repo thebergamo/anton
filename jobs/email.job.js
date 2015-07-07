9324e2843ab1afefe53136c37cdd08068140c20b
Marcos Bérgamo <markusdarkness@gmail.com>
2015-07-07 00:42:29 -0300

    Sengrid: Add sengrid transporter.

--- jobs/email.job.js
+++ jobs/email.job.js
@@ -2,17 +2,47 @@ module.exports = function(Queue){
 	email = Queue('email');
 
 	email.process(function(job, done){
-		if(job.data.email == 'marcos@thedon.com.br'){
-			job.progress('Error');
-			done(new TypeError('Holy shit!'));
+		var mustache = require('mustache');
+		var sendgrid = require('sendgrid')(job.data.credentials.sendgrid_api_key);
+
+		job.progress('Running');
+		
+		var to = job.data['to'] || [];
+		if(!to || to.length == 0){
+			throw new TypeError('You must sent least one email to delivery');
 		}
 
-		job.progress('Email are sent');
-		job.data.results = {
-			returnable: 'All emails are sent'
-		};
+		var from = job.data['from'] || 'no-reply@antonproj.com';
+		var subject = job.data['subject'] || "";
+		var template = job.data['template'];
+		var data = job.data['data'];
 
-		setTimeout(done, 1000);
+		if(!template || template.length == 0){
+			throw new TypeError('You must sent email template');
+		}
+
+		mustache.parse(template);
+		var rendered = mustache.render(template, data);
+		
+		console.log(rendered);
+
+		var myEmail = new sendgrid.Email({
+			to: to,
+			from: from,
+			subject: subject,
+			text: rendered
+		});
+
+		sendgrid.send(myEmail, function(err, json){
+			if(err){
+				throw err;	
+			}
+
+			job.data.results = { returnable: json };
+			job.progress('Done');
+			done();
+		});
+
 	});
 
 	return email;
