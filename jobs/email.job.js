d3d34f3611f620cea169f91547f7847a924c38c3
Marcos Bérgamo <markusdarkness@gmail.com>
2015-07-15 10:37:30 -0300

    Email job: change for sendgrid-mustacher.

--- jobs/email.job.js
+++ jobs/email.job.js
@@ -1,48 +1,27 @@
+var _ = require('lodash');
+var Promise = require('bluebird');
+var SendgridMustacher = require('sendgrid-mustacher');
+
 module.exports = function(Queue){
-	email = Queue('email');
+	var email = Queue('email');
 
 	email.process(function(job, done){
-		var mustache = require('mustache');
-		var sendgrid = require('sendgrid')(job.data.credentials.sendgrid_api_key);
-
+		var sendgrid = new SendgridMustacher(job.data.credentials.sendgrid_api_key);
+		var raw = job.data['email'];
+		var data = raw['data'];
+		console.log(raw);
 		job.progress('Running');
-		
-		var to = job.data['to'] || [];
-		if(!to || to.length == 0){
-			throw new TypeError('You must sent least one email to delivery');
-		}
 
-		var from = job.data['from'] || 'no-reply@antonproj.com';
-		var subject = job.data['subject'] || "";
-		var template = job.data['template'];
-		var data = job.data['data'];
-
-		if(!template || template.length == 0){
-			throw new TypeError('You must sent email template');
-		}
-
-		mustache.parse(template);
-		var rendered = mustache.render(template, data);
-		
-		console.log(rendered);
-
-		var myEmail = new sendgrid.Email({
-			to: to,
-			from: from,
-			subject: subject,
-			text: rendered
-		});
-
-		sendgrid.send(myEmail, function(err, json){
-			if(err){
-				throw err;	
-			}
-
-			job.data.results = { returnable: json };
+		return Promise
+		.bind(this)
+		.then(function(){
+			return sendgrid.sendBatch(raw, data);
+		})
+		.then(function(json){
+			job.data.results = { returnable: json};
 			job.progress('Done');
-			done();
-		});
-
+		})
+		.finally(done);
 	});
 
 	return email;
